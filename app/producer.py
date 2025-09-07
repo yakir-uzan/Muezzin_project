bbd12c951885ce212a2b50b233704af02359d2af
Yakir Uzan <yakiruzangreen@gmail.com>
2025-09-07 16:22:52 +0300

    add: producer that receives the json and inserts it into a topic in Kafka

---
+++ app/producer.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+import os
+from dotenv import load_dotenv
+import json
+from loader import Loader
+
+load_dotenv()
+
+class Producer:
+    def __init__(self):
+        self.topic = os.getenv("KAFKA_TOPIC")
+        self.producer = KafkaProducer(
+            bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS"),
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"))
+
+    def publish(self, message):
+        self.producer.send(self.topic, message)
+        self.producer.flush()
+        print("הטעינה הושלמה!")
+
+p = Producer()
+p.publish(Loader().get_metadata())
