af0932f7f1ddcc4a7caa8d86f65732a11f3b56ec
Yakir Uzan <yakiruzangreen@gmail.com>
2025-09-08 17:59:32 +0300

    add: logger for part fils

--- app/loader.py
+++ app/loader.py
@@ -3,23 +3,33 @@ import pathlib
 import os
 from dotenv import load_dotenv
 import datetime
+from logger import Logger
 
 load_dotenv()
+logger = Logger.get_logger()
 
 class Loader:
     def __init__(self):
         self.path_env = os.getenv("PATH_FILE")
         self.path = pathlib.Path(self.path_env)
         self.all_path = [item for item in self.path.iterdir() if item.is_file()]
+        logger.info(f"Loader with path: {self.path_env}")
+        logger.debug(f"Files found: {[file.name for file in self.all_path]}")
 
     def get_metadata(self):
         list_metadata = []
         for file in self.all_path:
-            metadata = {
-                "file_name": file.name,
-                "file_path": str(file.resolve()),
-                "file_size_bytes": file.stat().st_size,
-                "created_at": datetime.datetime.fromtimestamp(file.stat().st_ctime).isoformat()
-            }
-            list_metadata.append(metadata)
+            try:
+                metadata = {
+                    "file_name": file.name,
+                    "file_path": str(file.resolve()),
+                    "file_size_bytes": file.stat().st_size,
+                    "created_at": datetime.datetime.fromtimestamp(file.stat().st_ctime).isoformat()}
+
+                list_metadata.append(metadata)
+                logger.debug(f"Metadata extracted for file: {file.name}")
+
+            except Exception as e:
+                logger.error(f"Failed to extract metadata from {file.name}: {e}")
+            logger.info(f"Extracted metadata for {len(list_metadata)} files.")
         return list_metadata
---
+++ logger.py
@@ -0,0 +1,37 @@
+import logging
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+
+class Logger:
+    _logger = None
+
+    @classmethod
+    def get_logger(cls, name="muezzin_logger", es_host="http://localhost:9200", index="muezzin_logs", level=logging.DEBUG):
+        if cls._logger:
+            return cls._logger
+
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+
+        if not logger.handlers:
+            es = Elasticsearch(es_host)
+
+            class ESHandler(logging.Handler):
+                def emit(self, record):
+                    try:
+                        es.index(index=index, document={
+                            "timestamp": datetime.utcnow().isoformat(),
+                            "level": record.levelname,
+                            "logger": record.name,
+                            "message": record.getMessage()
+                        })
+                    except Exception as e:
+                        print(f"[Logger] Failed to log to Elasticsearch: {e}")
+
+
+            logger.addHandler(ESHandler())
+            logger.addHandler(logging.StreamHandler())
+
+        cls._logger = logger
+        return logger
