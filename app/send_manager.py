e3ad1ba051cad59f212defd1b01eeb489c15f553
Yakir Uzan <yakiruzangreen@gmail.com>
2025-09-08 17:14:43 +0300

    add: mongo dal

--- app/send_manager.py
+++ app/send_manager.py
@@ -1,6 +1,9 @@
 from loader import Loader
 from producer import Producer
 from processor import Processor
+from dal.elasticDAL import ElasticDAL
+from dal.mongoDAL import MongoDAL
+
 
 def main():
     # יצירת מופע ללאודר והשמת המטאדאטה במשתנה
@@ -16,13 +19,25 @@ def main():
     # יצירת איידיז לכל המטאדאטה
     processor = Processor()
     enriched = []
-
     for item in metadata:
         item["id"] = processor.generate_id(item)
         enriched.append(item)
 
+    # for item in enriched:
+    #     print(item)
+
+    # שליחה לאלסטיק
+    elastic = ElasticDAL()
     for item in enriched:
-        print(item)
+        elastic.upload(item)
+    print("upload all items to elastic")
+
+    # שליחה למונגו
+    mongo = MongoDAL()
+    for item in enriched:
+        mongo.upload_file(item["file_path"], item["id"])
+    print("upload all items to mongoDB")
+
 
 if __name__ == "__main__":
     main()
